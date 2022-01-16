f8546866ce48ad12801682dd19c718f47739c0a4
Peter Chapin <chapinp@acm.org>
2022-01-16 13:35:55 -0500

    Update hello.adb
    Added a use statement to the Hello program.

--- samples/hello.adb
+++ samples/hello.adb
@@ -6,8 +6,9 @@
 ---------------------------------------------------------------------------
 
 with Ada.Text_IO;
+use Ada.Text_IO;
 
 procedure Hello is
 begin
-  Ada.Text_IO.Put_Line("Hello, Ada!");
+  Put_Line("Hello, Ada!");
 end Hello;
