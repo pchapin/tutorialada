18653222a6c0f54154fe3d3d63435d83d305b472
Peter Chapin <chapinp@acm.org>
2022-05-11 09:54:48 -0400

    Some enhancements to the filter demonstration program.

--- samples/filter.adb
+++ samples/filter.adb
@@ -8,18 +8,29 @@
 -- whatever application is needed by expanding the processing done inside the while loop.
 ---------------------------------------------------------------------------
 
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO;
+with Ada.Integer_Text_IO;
+
+use Ada.Text_IO;
+use Ada.Integer_Text_IO;
 
 procedure Filter is
    -- Declare local variables and subprograms here.
+   Line_Number : Positive := 1;
 begin
    -- Read the entire input one line at a time...
    while not End_Of_File(Standard_Input) loop
       declare
-         Line : String := Get_Line;  -- Function Ada.Text_IO.Get_Line gets a complete line.
+         -- Function Ada.Text_IO.Get_Line gets a complete line of any length.
+         -- Normally strings need to be sized when they are declared. Here the size is taken
+         -- from the value returned by Get_Line, so we don't need to know the size ahead of time.
+         -- However, it is necessary to declare Line here, which is why a new 'declare' block
+         -- was created.
+         Line : constant String := Get_Line;
       begin
          -- Include whatever processing of Line is desired.
-         Put_Line(Line);
+         Put(Line_Number); Put(": "); Put_Line(Line);
+         Line_Number := Line_Number + 1;
       end;
    end loop;
 end Filter;
