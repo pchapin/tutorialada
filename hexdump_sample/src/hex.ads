4402b0cafc2d44c4f38f4e70ec44f99718572f8a
Peter Chapin <2094217+pchapin@users.noreply.github.com>
2025-01-14 20:59:24 -0500

    Restyled the Hexdump sample.

--- hexdump_sample/src/hex.ads
+++ hexdump_sample/src/hex.ads
@@ -1,15 +1,15 @@
 ---------------------------------------------------------------------------
---  FILE   : hex.ads
---  SUBJECT: Specification of a package for manipulating hex strings.
+-- FILE   : hex.ads
+-- SUBJECT: Specification of a package for manipulating hex strings.
 --
 ---------------------------------------------------------------------------
 
 with Interfaces;
 
 package Hex is
-   --  Convert an 8 bit quantity to a two digit hex string.
-   function To_String (Value : Interfaces.Unsigned_8) return String;
+   -- Convert an 8 bit quantity to a two digit hex string.
+   function To_String(Value : Interfaces.Unsigned_8) return String;
 
-   --  Convert a 32 bit quantity to a four digit hex string.
-   function To_String (Value : Interfaces.Unsigned_32) return String;
+   -- Convert a 32 bit quantity to a four digit hex string.
+   function To_String(Value : Interfaces.Unsigned_32) return String;
 end Hex;
--- hexdump_sample/src/hexdump_sample.adb
+++ hexdump_sample/src/hexdump_sample.adb
@@ -1,6 +1,6 @@
 ---------------------------------------------------------------------------
---  FILE   : hexdump_sample.adb
---  SUBJECT: Simple hex file viewer utility.
+-- FILE   : hexdump_sample.adb
+-- SUBJECT: Simple hex file viewer utility.
 --
 ---------------------------------------------------------------------------
 
@@ -11,9 +11,9 @@ with Interfaces; use Interfaces;
 with Hex;
 
 procedure Hexdump_Sample is
-   package Byte_IO is new Ada.Sequential_IO (Unsigned_8);
+   package Byte_IO is new Ada.Sequential_IO(Unsigned_8);
 
-   procedure Process_File (Name : String) is
+   procedure Process_File(Name : String) is
       Input_File : Byte_IO.File_Type;
       Item       : Unsigned_8;
       Item_Count : Unsigned_32 := 0;
@@ -21,57 +21,57 @@ procedure Hexdump_Sample is
       subtype Line_Size_Type is Integer range 0 .. 16;
       Line_Size  : Line_Size_Type := 0;
 
-      Text_Form  : String (1 .. 16) := (others => '.');
+      Text_Form  : String(1 .. 16) := (others => '.');
 
    begin -- Process_File
-      Byte_IO.Open (Input_File, Byte_IO.In_File, Name);
-      while not Byte_IO.End_Of_File (Input_File) loop
-         Byte_IO.Read (Input_File, Item);
+      Byte_IO.Open(Input_File, Byte_IO.In_File, Name);
+      while not Byte_IO.End_Of_File(Input_File) loop
+         Byte_IO.Read(Input_File, Item);
 
-         --  If at the beginning of the line, print file offset.
+         -- If at the beginning of the line, print file offset.
          if Line_Size = 0 then
-            Put (Hex.To_String (Item_Count));
-            Put (": ");
+            Put(Hex.To_String(Item_Count));
+            Put(": ");
          end if;
 
-         --  Print item.
+         -- Print item.
          Item_Count := Item_Count + 1;
          Line_Size  := Line_Size + 1;
          if Item >= 16#20# and Item <= 16#7E# then
-            Text_Form (Line_Size) := Character'Val (Item);
+            Text_Form(Line_Size) := Character'Val(Item);
          end if;
-         Put (Hex.To_String (Item));
+         Put(Hex.To_String(Item));
          if Line_Size = 8 then
-            Put (" - ");
+            Put(" - ");
          else
-            Put (" ");
+            Put(" ");
          end if;
 
-         --  Go to next line if necessary.
+         -- Go to next line if necessary.
          if Line_Size = Line_Size_Type'Last then
-            Put ("|"); Put (Text_Form); Put ("|"); New_Line;
+            Put("|"); Put(Text_Form); Put("|"); New_Line;
             Line_Size := Line_Size_Type'First;
             Text_Form := (others => '.');
          end if;
       end loop;
-      Byte_IO.Close (Input_File);
+      Byte_IO.Close(Input_File);
    end Process_File;
 
 begin -- Hexdump_Sample
 
-   --  Check command line.
+   -- Check command line.
    if Argument_Count = 0 then
-      Put ("Usage: ");
-      Put (Command_Name);
-      Put (" filename ...");
+      Put("Usage: ");
+      Put(Command_Name);
+      Put(" filename ...");
       New_Line;
-      Set_Exit_Status (Failure);
+      Set_Exit_Status(Failure);
    else
       --  Process each file name on the command line.
       for I in 1 .. Argument_Count loop
-         Put ("Dump of "); Put (Argument (I)); New_Line (2);
-         Process_File (Argument (I));
-         New_Line (2); Put ("end"); New_Line;
+         Put("Dump of "); Put(Argument(I)); New_Line(2);
+         Process_File(Argument(I));
+         New_Line(2); Put("end"); New_Line;
       end loop;
    end if;
 
